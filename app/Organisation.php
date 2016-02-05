27f82d194f365b2cc9c53fca205e69a815052145
Amy Garde <Amy@Amy-Gardes-MacBook.local>
2016-02-05 13:04:11 +0000

    removed organisations model and deleted extra create_organisations_table.php

--- app/Organisation.php
+++ app/Organisation.php
@@ -4,6 +4,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organisation extends Model {
 
-	//
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'organisations';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'bio', 'image', 'scope'];
 
 }
