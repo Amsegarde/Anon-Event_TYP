0638395d42227ad2ced012b908d14871d907e463
jonnyom <jonathanomahony@gmail.com>
2016-02-20 13:52:46 +0000

    Slightly more updated for DB's. Should all work now

--- app/Event.php
+++ app/Event.php
@@ -4,6 +4,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model {
 
-	//
-
+	protected $fillable = ['name', 
+						'bio',
+						'image',
+						'start_date',
+						'end_date',
+						'location',
+						'no_tickets',
+						'avail_tickets',
+						'price', 
+						'genre',
+						'keywords/tags',
+						'active', 
+						'scope'];
 }
