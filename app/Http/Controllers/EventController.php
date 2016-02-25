3606555a1ae3895f478351f3a8eff3a9c5d703f5
Amy Garde <Amy@Amy-Gardes-MacBook.local>
2016-02-25 15:16:32 +0000

    improved browse filter

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -222,39 +222,29 @@ class EventController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function browse(){
+	public function browse(Request $request){
 		//needs users filters and scope filters and some kind of algorithm
 		//needs to be relevant events not just all.
-			$events = Event::all();
-			$search = Event::all();
-			return view('events.browse', compact('events', 'search'));
-	}
-
-	/**
-	 * Apply the filters to the browse
-	 *
-	 * @return Response
-	 */
-	public function filter(Request $request) {
 		$events = new Event;
 
-		if ($request->location){
-			$events = $events->where('location', '=', $request->location)->get();
-		}
+		if (!empty($request->all())) {
+			if ($request->location){
+				$events = $events->where('location', '=', $request->location)->get();
+			}
 
-		if ($request->genre) {
-			$events = Event::where('genre','=', $request->genre)->get();
-		}
+			if ($request->genre) {
+				$events = Event::where('genre','=', $request->genre)->get();
+			}
 
-		if ($request->price) {
-			$events = Event::where('price', '<=', $request->price)->get();
-		}
-
+			if ($request->price) {
+				$events = Event::where('price', '<=', $request->price)->get();
+			}
+		} else {
+			$events = Event::all();
+		}	
+		
 		$search = Event::all();
-
-		return redirect('events')->with(compact('events','search'));
-		// return redirect('events')->with(array($search, $events));
-		// return redirect::action('',array('events','search'));
+		return view('events.browse', compact('events', 'search'));
 	}
 
 	public function show($id) {
