6e363154d71560576d9f70fce566101e1338caa8
Sean Cronin <djseancronin@gmail.com>
2016-03-07 16:04:53 +0000

    Hello

--- resources/views/users/account.blade.php
+++ resources/views/users/account.blade.php
@@ -2,12 +2,6 @@
 
 @section('content')
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-
-				
 				<div class="row">
 					<p><?php 
 						echo Session::get('message');
@@ -113,8 +107,5 @@
 
 						</script>
 				@endif
-			</div>
-		</div>
-	</div>
-</div>
+
 @endsection
